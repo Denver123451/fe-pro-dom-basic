c0b3e52e03df3b2c375061310a7b71a3caa79186
Denver12345 <villaan12345>
2022-07-09 15:18:19 +0300

    Done!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-export const paintCards = () => {};
+export const paintCards = () => {
+  const items = document.querySelectorAll('.item:nth-child(odd)');
+  items.forEach((item) => (item.style.backgroundColor = 'red'));
+};
 
-export const findElement = () => {};
+export const findElement = () => {
+  let item = document.querySelector('li');
+  while (item) {
+    if (item.matches('.likedItem')) {
+      item.style.backgroundColor = 'blue';
+      break;
+    } else {
+      item = item.nextElementSibling;
+    }
+  }
+};
