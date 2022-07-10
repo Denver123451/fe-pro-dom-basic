00d38d9d401455163b3b627cbaaf22fdc0fc32d3
Denver12345 <villaan12345>
2022-07-10 20:04:18 +0300

    Done!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 export const paintCards = () => {
-  //const items = document.querySelectorAll('.item:nth-child(odd)'); не люблю я сразу добираться к искомому
-  //items.forEach((item) => (item.style.backgroundColor = 'red'));
   document.querySelectorAll('.item').forEach((el, num) => {
     if (num % 2 !== 0) {
       el.style.backgroundColor = 'red';
